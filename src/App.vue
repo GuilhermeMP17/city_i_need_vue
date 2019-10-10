25e4d7c1b2846c1d5586a23f3294aac25abb85ff
GuilhemeMP17 <guilherme.pereira17@outlook.com>
2019-10-10 08:19:34 -0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<Comments/>
+</template>
+
+<script>
+import Comments from './components/Comments.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Comments
+  }
+}
+</script>
+
+<style>
+</style>
